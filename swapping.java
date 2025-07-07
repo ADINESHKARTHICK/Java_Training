a438b8641c591bfbe3d949c0a36e0ad56979ae12
ADINESHKARTHICK <dinesha1406@gmail.com>
2025-07-07 09:28:58 +0530

    swapping.java

---
+++ swapping.java
@@ -0,0 +1,21 @@
+package Dinesh;
+import java.util.*;
+public class swapping {
+	 public static void main(String args[]){
+	        Scanner sc=new Scanner(System.in);
+	        int n=sc.nextInt();
+	        int a[]=new int[n];
+	        for(int i=0;i<n;i++){
+	            a[i]=sc.nextInt();
+	        }
+	        for(int i=0;i<n/2;i++){
+	            int t=a[i];
+	            a[i]=a[n-i-1];
+	            a[n-i-1]=t;
+	        
+	        }
+	        for(int i=0;i<n;i++){
+	            System.out.print(a[i]);
+	        }
+	    }
+}
